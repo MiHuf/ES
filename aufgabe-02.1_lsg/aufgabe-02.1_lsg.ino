b47fcaabe300b884ab161d23413b4b8cafa912f0
MiHuf <michael@hufschmidt-web.de>
2015-04-17 11:30:50 +0200

    Divisor eingebaut

--- aufgabe-02.1_lsg/aufgabe-02.1_lsg.ino
+++ aufgabe-02.1_lsg/aufgabe-02.1_lsg.ino
@@ -3,6 +3,16 @@
 //            Tim Welge            tw@ens-fiti.de
 //            Rania Wittenberg     rania_wittenberg@hotmail.com
 
+
+// Timer Params
+uint32_t dwFreq = 1000;  // ???
+uint32_t dwMCk = 0;  // ???
+uint32_t dwBoardMCK = 84000000;  // ???
+uint32_t *dwDiv;
+uint32_t *dwTcClks;
+uint32_t mckDivisor;
+uint32_t dwMode = 0; // ???
+
 void setup() {
   pmc_set_writeprotect(false);
   pmc_enable_periph_clk(ID_TC6);
@@ -18,9 +28,11 @@ void setup() {
   * \param mode  Operating mode (TC_CMR value).
   * extern void TC_Configure( Tc *pTc, uint32_t dwChannel, uint32_t dwMode )
   */
-  TC_Configure(TC2, 0, 0);
+  TC_Configure(TC2, 0, dwMode);
   NVIC_ClearPendingIRQ(TC6_IRQn);
   NVIC_EnableIRQ(TC6_IRQn);
+  mckDivisor = TC_FindMckDivisor(dwFreq, dwMCk, dwDiv, dwTcClks, dwBoardMCK);
+  TC_SetRC(TC2, 0, mckDivisor);
 
   // >> Hier wird der konfigurierte Timer gestartet
   /**
@@ -42,6 +54,10 @@ void loop() {
 }
 
 void TC6_Handler() {
+  uint32_t stat, value;
+  stat = TC_GetStatus(TC2, 0);
+  value = TC_ReadCV(TC2, 0);
   // tu was
+  
 
 }
