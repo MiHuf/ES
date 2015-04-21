cabb0a4f9bc39f93384f74ad9fd1bdc1b7d0e4c5
MiHuf <michael@hufschmidt-web.de>
2015-04-21 11:40:20 +0200

    Dienstag 11:40 Timer Interrupt funktioniert

--- aufgabe-02.1_lsg/aufgabe-02.1_lsg.ino
+++ aufgabe-02.1_lsg/aufgabe-02.1_lsg.ino
@@ -10,41 +10,50 @@ const uint32_t dwChannel = 0;
 // WAVSEL (bits 13, 14)  = 2 = UPRC,
 // WAVE (bit 15) = 1 = ENABLED
 const uint32_t dwMode = 0b000 | 0b10 << 13 | 0b1 << 15;  // = 49152 = C000
+uint32_t timerValue = 0;                                 // Millisekunden
 
 
 // Other Params and Variables
 const int led = 13;  // interne LED
-boolean ledOn = false;
+const int swli = 5;
+const int swre = 7;
+boolean ledOn = true;
 
 void setup() {
   // Timer setup and start
   pmc_set_writeprotect(false);
-  pmc_enable_periph_clk(ID_TC6);
+  pmc_enable_periph_clk(ID_TC6);                   // Timer 2, channel 0
   TC_Configure(TC2, dwChannel, dwMode);
-  NVIC_ClearPendingIRQ(TC6_IRQn);  // Timer 2, channel 0
-  NVIC_EnableIRQ(TC6_IRQn);        // Timer 2, channel 0
-  TC_SetRC(TC2, dwChannel, 41999); // (84 MHz / 2) - 1 Hz
+  TC2->TC_CHANNEL[dwChannel].TC_IER = 0b1<<4;      // siehe Datenblatt Seite 917
+  TC2->TC_CHANNEL[dwChannel].TC_IDR = ~(0b1<<4);   // siehe Datenblatt Seite 918
+  NVIC_ClearPendingIRQ(TC6_IRQn);                  // Timer 2, channel 0
+  NVIC_EnableIRQ(TC6_IRQn);                        // Timer 2, channel 0
+  TC_SetRC(TC2, dwChannel, 41999);                 // (84 MHz / 2 - 1 Hz) set to 1 kHz
   TC_Start(TC2, dwChannel);
   // Other setup
   pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
+  digitalWrite(led, ledOn);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if ((timerValue % 1000) == 0) {  // alle 1000 ms
+    Serial.println(timerValue);
+  }
 }
 
 void switchLed() {
-  ledOn = !ledOn;
+  ledOn = not(ledOn);
   digitalWrite(led, ledOn);
 }
 
 void TC6_Handler() {
-  uint32_t stat, value;
-  stat = TC_GetStatus(TC2, 0);
-  value = TC_ReadCV(TC2, 0);
+  uint32_t stat;
+  stat = TC_GetStatus(TC2, dwChannel);
+  timerValue = timerValue + 1;
   // tu was
-
-
+  if ((timerValue % 200) == 0) {  // alle 200 ms
+    switchLed();
+  }
 }
