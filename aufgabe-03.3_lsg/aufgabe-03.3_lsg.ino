10310d173b70d114a9b4913ce3c2ec6eda07be4d
Michael Hufschmidt <michael@hufschmidt-web.de>
2015-05-05 17:44:30 +0200

    Gerüst für Aufgabe 3.2 ... 3.5

---
+++ aufgabe-03.3_lsg/aufgabe-03.3_lsg.ino
@@ -0,0 +1,14 @@
+// Aufgabe 3.3, Stand von 2015-05-05
+// Lösung von Michael Hufschmidt   michael@hufschmidt-web.de,
+//            Tim Welge            tw@ens-fiti.de
+//            Rania Wittenberg     rania_wittenberg@hotmail.com
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
