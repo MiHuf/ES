a2ed08105fa4eb14037d74f022723275067f3530
MiHuf <michael@hufschmidt-web.de>
2015-05-05 11:40:45 +0200

    im Prinzip fertig, noch nicht testiert

--- aufgabe-03.1_lsg/aufgabe-03.1_lsg.ino
+++ aufgabe-03.1_lsg/aufgabe-03.1_lsg.ino
@@ -9,13 +9,14 @@ const int pinX2 = 4;
 const int pinZ2 = 5;
 const int pinRef = 6;
 
-const int xSens = 2;        //Sensitivity x-Out
-const int zSens = 2;        //Sensitivity z-Out
-const double x1Sens = 9.1;  //Sensitivity x4.5Out
-const double z1Sens = 9.1;  //Sensitivity z4.5Out
+const double x1Sens = 2;    // Sensitivity x-Out
+const double z1Sens = 2;    // Sensitivity z-Out
+const double x2Sens = 9.1;  // Sensitivity x4.5Out
+const double z2Sens = 9.1;  // Sensitivity z4.5Out
 int x1Rate, z1Rate;         // x-Out, z-Out
 int x2Rate, z2Rate;         // x4.5Out , z4.5Out
-int VREF;
+int zeit = 3000;            // Delay 1 s
+int Vref;
 
 
 
@@ -28,36 +29,36 @@ void loop() {
   // put your main code here, to run repeatedly:
   x1Rate = analogRead(pinX1);      // Liest Analog input pin 2
   z1Rate = analogRead(pinZ1);      // Liest Analog input pin 3
-  x2Rate = analogRead(pinX2);     // Liest Analog input pin 4
-  z2Rate = analogRead(pinZ2);     // Liest Analog input pin 5
-  VREF = analogRead(pinRef);       // Liest Analog input pin 6
+  x2Rate = analogRead(pinX2);      // Liest Analog input pin 4
+  z2Rate = analogRead(pinZ2);      // Liest Analog input pin 5
+  Vref = analogRead(pinRef);       // Liest Analog input pin 6
 
-  Serial.println("Rotation x,z:");
-  Serial.println(x1Rate, DEC);   // Ausgabe Rotation  in Achse  X
-  Serial.println(" ");          // leertaste
-  Serial.println(z1Rate, DEC);   // Ausgabe Rotation  in Achse  Z
-  delay(100);                   // Warten  100ms vor die nächste Lesung
+  Serial.print("Rotation x1 ,z1: ");
+  Serial.print(x1Rate, DEC);    // Ausgabe Rotation  in Achse  X
+  Serial.print(", ");           // leertaste
+  Serial.print(z1Rate, DEC);    // Ausgabe Rotation  in Achse  Z
+  Serial.print("\nWinkelgeschindigkeit x1-Out, z1-Out: ");
+  Serial.print(winkelgeschwindigkeit(x1Rate, Vref, x1Sens), DEC);
+  Serial.print(", ");
+  Serial.print(winkelgeschwindigkeit(z1Rate, Vref, z1Sens), DEC);
+  Serial.print("\n");           // neue Zeile
+  Serial.print("\n");           // neue Zeile
 
-  Serial.println("Gyro Rotation x-Out: ");
-  Serial.println(gyroRotationRate((double) x1Rate, (double) VREF, (double) xSens), DEC);
-  Serial.println("\n ");
-  Serial.println("Gyro Rotation z-Out: ");
-  Serial.println(gyroRotationRate((double) z1Rate, (double) VREF, (double) zSens), DEC);
+  Serial.print("Rotation x2 ,z2: ");
+  Serial.print(x2Rate, DEC);    // Ausgabe Rotation  in Achse  X
+  Serial.print(", ");           // leertaste
+  Serial.print(z2Rate, DEC);    // Ausgabe Rotation  in Achse  Z
+  Serial.print("\nWinkelgeschindigkeit x2-Out, z2-Out: ");
+  Serial.print(winkelgeschwindigkeit(x2Rate, Vref,  x2Sens), DEC);
+  Serial.print(", ");
+  Serial.print(winkelgeschwindigkeit(z2Rate, Vref,  z2Sens), DEC);
+  Serial.print("\n");           // neue Zeile
+  Serial.print("\n");           // neue Zeile
 
-  Serial.println(x2Rate, DEC);  // Ausgabe Rotation  x4.5  in Achse  X
-  Serial.println(" ");          // leertaste
-  Serial.println(z2Rate, DEC);  // Ausgabe Rotation z4.5  in Achse  Z
-  delay(100);                   // Warten  100ms vor die nächste Lesung
-  Serial.println("\n ");
-
-  Serial.println("Gyro Rotation x4.5Out: ");
-  Serial.println(gyroRotationRate((double) x2Rate, (double) VREF, x1Sens), DEC);
-  Serial.println("\n ");
-  Serial.println("Gyro Rotation z4.5Out: ");
-  Serial.println(gyroRotationRate((double) z2Rate, (double) VREF, z1Sens), DEC);
+  delay(zeit);                   // Warten  100ms vor die nächste Lesung
 }
 
-double gyroRotationRate(double voltage, double zero, double sensitiv) {
-  // Gyro rotation rate=(Gyro Output Voltage - Gyro Zero-Rate Out) / Sensitivity
-  return (voltage - zero) / sensitiv;
+double winkelgeschwindigkeit(int voltage, int zero, double sensitiv) {
+  //Formel gemäß Seite 13 im Datenblatt
+  return ((double) voltage - (double) zero) / sensitiv;
 }
