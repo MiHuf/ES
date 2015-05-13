6d49663e8106183595a451e6dfde270461d1a06d
MiHuf <michael@hufschmidt-web.de>
2015-05-13 11:56:08 +0200

    Fertig

--- aufgabe-03.4_lsg/aufgabe-03.4_lsg.ino
+++ aufgabe-03.4_lsg/aufgabe-03.4_lsg.ino
@@ -1,32 +1,35 @@
-  // Aufgabe 3.4, Stand von 2015-05-13
-  // Lösung von Michael Hufschmidt   michael@hufschmidt-web.de,
-  //            Tim Welge            tw@ens-fiti.de
-  //            Rania Wittenberg     rania_wittenberg@hotmail.com
-  
-  String readString;
-  int swli = 4;
-  int swre = 9;
-  int led = 13;
-  void setup() {
-    pinMode(led, OUTPUT);
+// Aufgabe 3.4, Stand von 2015-05-13
+// Lösung von Michael Hufschmidt   michael@hufschmidt-web.de,
+//            Tim Welge            tw@ens-fiti.de
+//            Rania Wittenberg     rania_wittenberg@hotmail.com
+
+String readString;
+int swli = 4;
+int swre = 9;
+int led = 13;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  while (Serial.available()) {
+    char c = Serial.read();          //gets one byte from serial buffer
+    if (c != '\n') {
+      readString += c;
+    } else {
+      if (readString.length() > 0) {
+        Serial.println(readString);  //so you can see the captured string
+        if (readString == "LedOn") {
+          digitalWrite(led, HIGH);
+        }
+        if (readString == "LedOff") {
+          digitalWrite(led, LOW);
+        }
+        readString = "";
+      }
     }
-  
-  void loop() {
-   while (Serial.available()) {
-      char c = Serial.read();  //gets one byte from serial buffer
-      readString += c; 
-      delay(2);  
-    }
-    
-    if (readString.length() >0) {
-      Serial.println(readString);  //so you can see the captured string 
-      
-    if(readString=="LedON"){
-         digitalWrite(led, HIGH);
-    }
-    if(readString=="LedOff"){
-       digitalWrite(led, LOW);
-  
-    }
+    delay(2);
   }
-  }
+}
