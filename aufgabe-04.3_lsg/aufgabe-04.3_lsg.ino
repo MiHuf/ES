55fffd7da6f6cf83bcc7cdc20916bccf01179a2a
Michael Hufschmidt <michael@hufschmidt-web.de>
2015-06-07 11:25:32 +0200

    nicht benötigte Funktionen entfernt

--- aufgabe-04.3_lsg/aufgabe-04.3_lsg.ino
+++ aufgabe-04.3_lsg/aufgabe-04.3_lsg.ino
@@ -1,4 +1,4 @@
-// Aufgabe 4.3, Stand von 2015-06-05
+// Aufgabe 4.3, Stand von 2015-06-07
 // Lösung von Michael Hufschmidt   michael@hufschmidt-web.de,
 //            Tim Welge            tw@ens-fiti.de
 //            Rania Wittenberg     rania_wittenberg@hotmail.com
@@ -8,7 +8,9 @@
 
 // Pin Constants
 const int pinLed = 13;                  // internal LED
+const int pinIsMaster = 8;              // Pin 8 = 3,3 V for Master, = GND for Slave
 // I2C pins are SDA = 20, SCL = 21
+
 // for Gyro on Slave Board
 const int pinX1 = A2;                   // Analog Gyro XOUT
 const int pinZ1 = A3;                   // Analog Gyro ZOUT
@@ -16,20 +18,19 @@ const int pinX2 = A4;                   // Analog Gyro X4.5OUT
 const int pinZ2 = A5;                   // Analog Gyro Z4.5OUT
 const int pinRef = A6;                  // Analog Gyro VREF
 const int pinAZ = 7;                    // Digital I/O Gyro
-// factor = (5000.0 Volt / 1024.0 steps) / senitivity
+// factor = (5000.0 mV / 1024.0 steps) / senitivity in [mv / Grad / sec]
 // Winkelgeschwindigkeit in Grad / Sekunde = factor * (rate - vref)
 const double factor2 = (5000.0 / 1024.0) / 2.0;
 const double factor45 = (5000.0 / 1024.0) / 9.1;
+
 // for Servo on Master Board
 const int servoPin = 3;                 // Servo-Pin
 const int wmin = 24;                    // Servo-Grenzwert, ermittelt
 const int wmax = 159;                   // Servo-Grenzwert, ermittelt
 const double schwelle = 1.0;            // Schwellwert fuer Gyro-Ausgabe
-const double gain = 0.1;                // Verstaerkung fuer Gyro
 int welchesGyro = pinZ1;                // evtl. anpassen
 
 // Other Constants
-const bool isMaster = true;             // to be adapted before upload
 const int adrSlave = 17;                // my birthdate
 
 // Timer Params
@@ -40,6 +41,7 @@ const uint32_t dwChannel = 0;
 const uint32_t dwMode = 0b000 | 0b10 << 13 | 0b1 << 15;  // = 49152 = C000
 
 // Variables
+bool isMaster = true;                   // Hardware configured
 const int len = sizeof(int);            // length of line buffer
 byte inBuffer[len];                     // data bufferconst int len = 160;
 int pos = 0;                            // position within line buffer
@@ -78,6 +80,8 @@ void setup() {
   // Other setup
   pinMode(pinLed, OUTPUT);
   digitalWrite(pinLed, LOW);            // LED Off
+  pinMode(pinIsMaster, INPUT);
+  isMaster = digitalRead(pinIsMaster);
   if (isMaster) {
     Wire.begin();
   } else {
@@ -91,43 +95,9 @@ void loop() {
   // put your main code here, to run repeatedly:
 }  // end loop
 
-void updateServo() {
-  // Gyro auslesen, neuen Winkel berechnen und Servo einstellen
-  double omega;
-  omega = readGyro();                   // Winkelgeschwindigkeit Grad pro Sekunde
-  winkel = winkel + int (gain * omega + 0.5); // Geschwindigkeit angepassen
-  //   winkel = (wmax - wmin) / 2 ;
-  if (winkel < wmin) {
-    // setBlink();
-    winkel = wmin;
-  }
-  if (winkel > wmax) {
-    // setBlink();
-    winkel = wmax;
-  }
-  moveServo(winkel);
-}
-void moveServo(int w) {
-  // Servo auf den Winkel w einstellen
-  Serial.print("Winkel = ");
-  Serial.println(w);                    // Ausgabe zur Kontrolle
-  neuServo.write(w);                    // Servo einstellen
-}
-
-void setServo(int w) {
-  // set Servo on Master
-}
-
-int readGyro() {
-  int rate, vref;
-  vref = analogRead(pinRef);         // Liest Analog input Ref
-  rate = analogRead(welchesGyro);
-  return rate - vref;
-}
-
 double gyroToWinkel(int wint) {
-  // Gyro auslesen und verarbeiten, gibt die Winkelgeschwindigkeit
-  // in Grad pro Sekunde zurück, Ermittlung des Faktors siehe Zeilen 19, 20
+  // Ermittelt aus wint = rate - vref die Winkelgeschwindigkeit 
+  // in Grad pro Sekunde, Ermittlung des Faktors siehe Zeilen 20, 21
   double factor, omega;
   switch (welchesGyro) {
     case pinX1:
@@ -150,21 +120,44 @@ double gyroToWinkel(int wint) {
   return omega;
 }
 
+void setServo(int w) {
+  // set Servo on Master according to w = rate - vref
+  double omega;
+  omega = gyroToWinkel(w);              // Winkelgeschwindigkeit Grad pro Sekunde
+  winkel = winkel + int (0.1 * omega + 0.5); // increment 10x per second
+  if (winkel < wmin) {
+    // setBlink();
+    winkel = wmin;
+  }
+  if (winkel > wmax) {
+    // setBlink();
+    winkel = wmax;
+  }
+  Serial.print("Setting Winkel on Master = ");
+  Serial.println(winkel);                // Ausgabe zur Kontrolle
+  neuServo.write(winkel);                // Servo einstellen 
+}
+
+int readGyro() {
+  int rate, vref;
+  vref = analogRead(pinRef);            // read Analog input Ref
+  rate = analogRead(welchesGyro);       // read Analog Input ZOUT
+  return rate - vref;
+}
+
 int checkGyro() {
-  byte result;
-  // check Gyro on Slave
+  // check Gyro on Slave, returns rate - vref 
   int w = 0;
+  byte result;
   byte c;
-  Wire.beginTransmission(adrSlave);
-  Wire.write(0);                        // send value byte
-  result = Wire.endTransmission();      // stop transmitting
+//  Wire.beginTransmission(adrSlave);
+//  Wire.write(7);                        // send any byte
+//  result = Wire.endTransmission();      // stop transmitting
   Wire.requestFrom(adrSlave, sizeof(int));
-  // while (Wire.available()) {
   for (int i = 0; i < sizeof(int); i++) {
     c = Wire.read();
     inBuffer[sizeof(int) - i] = c;
   }  // end of for loop
-  // }
   w = (int) inBuffer;
   Serial.print("Master: ");
   Serial.println(w);
@@ -174,28 +167,25 @@ int checkGyro() {
 void slaveHandler(int howMany) {
   byte c;
   int w;
-  c = Wire.read();    //  1 byte
+//  c = Wire.read();                      // read one byte and ignore
   w = readGyro();
-  // Wire.beginTransmission (0);
+  Serial.print("Slave: ");
+  Serial.println(w);
   for (int i = 0; i < sizeof(int); i++) {
-    c = w & 0xFF;     // lowest byte
-    // Wire.write(c);
-    Wire.write(0);
+    c = w & 0xFF;                       // lowest byte first
+    Wire.write(c);
     inBuffer[i] = c;
     c = (w >> 8) & 0xFF;
   }
-  // Wire.endTransmission();
-  Serial.print("Slave: ");
-  Serial.println(w);
 }
 
 void TC6_Handler() {
-  int w;
+  int w;                                // = rate - vref
   uint32_t stat;
   stat = TC_GetStatus(TC2, dwChannel);
   timerValue = timerValue + 1;
   // tu was
-  if ((timerValue % 100) == 0) {        // alle 100 ms
+  if ((timerValue % 100) == 0) {        // every 100 ms
     if (isMaster) {
       w = checkGyro();
       setServo(w);
