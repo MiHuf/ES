98fdb9bb0b190828e69da661b7fa825c273e82ab
Michael Hufschmidt <michael@hufschmidt-web.de>
2015-06-08 18:44:08 +0200

    Gerüst für Aufgaben 5.x

---
+++ aufgabe-05.2_lsg/aufgabe-05.2_lsg.ino
@@ -0,0 +1,22 @@
+// Aufgabe 5.2, Stand von 2015-06-08
+// Lösung von Michael Hufschmidt   michael@hufschmidt-web.de,
+//            Tim Welge            tw@ens-fiti.de
+//            Rania Wittenberg     rania_wittenberg@hotmail.com
+
+#include <SPI.h>
+
+// Pin Constants
+const int pinRST = 5;                   // RST 
+const int pinDC = 6;                    // D/C
+const int pinLed = 13;                  // internal LED
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+} // end setup
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}  // end loop
+
