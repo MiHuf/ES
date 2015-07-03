29e4fb4500b8b1bf2927f51d9db9c0c3500ec052
MiHuf <michael@hufschmidt-web.de>
2015-07-03 11:52:32 +0200

    Länge und Breite geht, der Rest fehlt

--- aufgabe-07.1_lsg/aufgabe-07.1_lsg.ino
+++ aufgabe-07.1_lsg/aufgabe-07.1_lsg.ino
@@ -12,13 +12,12 @@ const int pinCS1 = 4;                       // SPI: SCE Chip Select 1  <<< for W
 // Serial 3 pins are pinTX3 = 14 and pinRX3 = 15
 const int pinLed = 13;                      // internal LED
 
-const char *fileName = "RaMiTi.dat";        // Output Filename
+const char *fileName = "RaMiTi.dat";        // Output Filename von Rania, Michael Tim
 
 // Variables
-const int lineBufferLen = 80;               // size of line buffer
+const int lineBufferLen = 512;              // size of line buffer
 char lineBuffer[lineBufferLen];             // input line buffer
-int linePos = 0;
-
+int pos = 0;                                // position within line buffer
 void setup() {
   // put your setup code here, to run once:
   bool sdOK;
@@ -38,6 +37,34 @@ void loop() {
   // put your main code here, to run repeatedly:
 }
 
+void parseGPS() {
+  // Analyse $GPGGA
+  String line = lineBuffer;
+  int commapos[14];
+  char * msg;                          //
+  uint8_t fix;                         // sattelite data valid
+  uint8_t sats = 0;                    // number of sattelites
+  uint8_t n;
+  double latitude, longitude;          // Position
+  byte checksum;
+  if (line.substring(0, 6).equals("$GPGGA")) {
+    commapos[0] = line.indexOf(",");
+    for (int i = 1; i < 14; i++) {
+      commapos[i] = line.indexOf(",", commapos[i - 1] + 1);
+    }
+    Serial.println(lineBuffer);
+    latitude = line.substring(commapos[1] + 1, commapos[2]).toFloat();
+    longitude = line.substring(commapos[3] + 1, commapos[4]).toFloat();
+    Serial.println(latitude, 6);
+    Serial.println(longitude, 6);
+  }
+}
+
+bool check(byte checksum) {
+  // validates the checksum info
+  return true;
+}
+
 void serialEvent() {
   // somebody wants to talk
 }
@@ -45,17 +72,14 @@ void serialEvent() {
 void serialEvent3() {
   // GPS wants to talk
   byte c;
-  /*
   if (Serial3.available()) {
     c = Serial3.read();
-    zeile[pos] = c;
-    pos ++;                             // ready for next char
+    lineBuffer[pos] = c;
+    pos ++;                                  // ready for next char
   }
   if (c == '\n') {
-    zeile[pos] = 0;                     // terminate String
+    lineBuffer[pos] = 0;                     // terminate String
     pos = 0;
-    parseCommand();
-    Serial.println(zeile);              // for testing
+    parseGPS();
   }
-  */
 }
